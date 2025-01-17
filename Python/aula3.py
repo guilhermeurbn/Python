27f98eed03e5e9ba81c3d788e6d05c1267d9f055
Guilherme Urbano <guilhermeurbano@MacBookPro.lan>
2025-01-17 19:37:08 +0000

    :tada:

--- Python/aula3.py
+++ Python/aula3.py
@@ -1,18 +1,18 @@
 """aula numero 54. exercicio"""
 
 #1º exercicio
-""" num = input("digite um numero inteiro: ")
+num = input("digite um numero inteiro: ")
 try:
     if (int(num) % 2 == 0):
         print(f"o numero: {num} é Par")
     else:
         print(f"o numero: {num} é Impar")
 except ValueError:
-    print(f"o numero: {num}, não é um numero inteiro") """
+    print(f"o numero: {num}, não é um numero inteiro")
 
 
 #2º exercicio
-""" hora = int(input("senhor me informa as horas: "))
+hora = int(input("senhor me informa as horas: "))
 if hora <= 11:
     print("Bom dia senhor!")
 elif hora <= 17:
@@ -20,11 +20,11 @@ elif hora <= 17:
 elif hora <= 23:
     print("Boa noite senhor!") 
 else:
-    print("digite uma hora exata!")"""
+    print("digite uma hora exata!")
+
 
 #3º exercicio
-
-""" nome = input("Qual seu nome: ")
+nome = input("Qual seu nome: ")
 
 if len(nome) <= 4:
     print("seu nome é Curto")
@@ -32,4 +32,3 @@ elif len(nome) <= 6:
     print("seu nome é Normal")
 else:
     print("seu nome é muito Grande")
-     """
