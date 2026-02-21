d461a928e2c8eccf8cef94acd1daad2ffa7f177c
guilherme <guiurbano11@gmail.com>
2026-02-21 03:24:46 +0000

    new features

--- Python/Aulas/aula7.py
+++ Python/Aulas/aula7.py
@@ -1,42 +1,66 @@
 #aula 07
 
+lista = []
+
+cores = {
+    "red": "\033[31m",
+    "green": "\033[32m",
+    "yellow": "\033[33m",
+    "blue": "\033[34m",
+    "magenta": "\033[35m",
+    "cyan": "\033[36m",
+    "reset": "\033[0m"
+}
+
+def color(texto, cor):
+    if cor in cores:
+        return cores[cor] + texto + cores["reset"]
+    return texto
 
 
-def find(lista, valor):
+def find(lista, old_name):
     i = 0
     while i < len(lista):
-        if lista[i] == valor:
+        if lista[i] == old_name:
             return True
         i += 1
     return (False)
 
-def lugar(lista, valor):
+def lugar(lista, old_name):
     i = 0
     while i < len(lista):
-        if lista[i] == valor:
+        if lista[i] == old_name:
             return i
         i += 1
 
-lista = ["guilherme", 22, "milena", 20, "jonh", 23]
-
-
 i = 0
-novo_usuario = input("gostaria de atualizar o nome ou a idade: ")
+usuario = " "
 
-while(1):
-    if "nome" in novo_usuario:
+while(usuario != "exit"):
+
+    usuario = input("\nO que voce gostaria de fazer\n \n\tMudar nome: \n"\
+                    "\tAdicionar nome:\n\tRemover nome:\n\tlista nomes:\n :")
+    
+    if "mudar" in usuario:
         old_name = input("qual seu nome atual: ")
         if find(lista, old_name):
-            lista[lugar(lista, old_name)] = input("Qual o novo nome: ")
-            break
+            lista[lugar(lista, old_name)] = str(input("Qual o novo nome: ")).lstrip().rstrip()
+            print(color(lista[-1], "blue"))
         else:
-            print("Esse nome nao existe")
+            print(color("\n\tNOT EXISTED NAME", "red"))
 
-    if "idade" in novo_usuario:
-        old_age = int(input("qual sua idade atual: "))
-        if find(lista, old_age):
-            lista[lugar(lista, old_age)] = int(input("Qual sua nova idade: "))
-            break
-        else:
-            print("Essa idade nao existe")
+    elif "adicionar" in usuario:
+        new_name = str(input("Qual vai ser o nome adicionado: ")).lstrip().rstrip()
+        lista.append(new_name)
+        print(color(lista[-1], "blue"))
+        print(color("NEW NAME", "blue"))
+        
+    elif "remover" in usuario:
+        rem_name = input("Qual nome pra remover: ").lstrip().rstrip()
+        if find(lista, rem_name):
+            lista.pop()
+            print(color("NOME DELETADO", "red"))
+    elif "lista" in usuario:
+        print(lista)
+        
 print(lista)
